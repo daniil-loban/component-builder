04069820b4acdb3792d308d8f58142d1ac099c73
daniil-loban <dungeon_lda@mail.ru>
2020-04-06 10:22:21 +0300

    Updates

---
+++ precache-manifest.84985a2a113c6441492ebc1843d2725c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e9c899cab2c0cf0d4ff6faaadc6140",
+    "url": "/component-builder/index.html"
+  },
+  {
+    "revision": "36eff27e3bcd4ba4383a",
+    "url": "/component-builder/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "da69149453a9c1ef53e9",
+    "url": "/component-builder/static/js/2.9ada0faa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/component-builder/static/js/2.9ada0faa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36eff27e3bcd4ba4383a",
+    "url": "/component-builder/static/js/main.4c57669c.chunk.js"
+  },
+  {
+    "revision": "eef69b39bcd49730cce6",
+    "url": "/component-builder/static/js/runtime-main.73350b07.js"
+  }
+]);
